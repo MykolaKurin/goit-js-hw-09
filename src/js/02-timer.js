1ebface00821adb369fd7cc31e5aa6570348645e
MykolaKurin <nvk1967@gmail.com>
2021-11-07 16:46:23 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -31,16 +31,42 @@ const options = {
 };
 flatpickr("#datetime-picker", options);
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+function checkTimeAvailable(calendarDate) {
+    selectedTime = calendarDate[0].getTime();
+    currentTime = Date.now();
+      if (selectedTime > currentTime) {
+        refs.startBtn.removeAttribute('disabled');
+      } else {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        const checkDisableBtn = refs.startBtn.hasAttribute('disabled');
+        if (!checkDisableBtn) {
+        refs.startBtn.setAttribute('disabled', true);
+        }
+      }
+}
 
+function onStartClick() {
+    refs.input.setAttribute('disabled', true);
+    refs.startBtn.setAttribute('disabled', true);
+    timerId = setInterval(() => {
+        currentTime = Date.now();
+        const deltaTime = selectedTime - currentTime;
+        if (selectedTime <= currentTime) {
+            clearInterval(timerId);
+            refs.notification.textContent = "Ура! Время пришло!";
+            return;
+        } 
+        const time = convertTime(deltaTime);
+        updateTime(time)
+    }, 1000)
+}
+
+function updateTime({ days, hours, minutes, seconds }) {
+refs.days.textContent = `${days}`;
+refs.hours.textContent = `${hours}`;
+refs.minutes.textContent = `${minutes}`;
+refs.seconds.textContent = `${seconds}`;
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
