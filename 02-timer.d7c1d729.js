5bde3de8ae5f9d5029408631b28cd601720d9b12
MykolaKurin <84984136+MykolaKurin@users.noreply.github.com>
2021-11-07 14:50:58 +0000

    Deploying to gh-pages from @ MykolaKurin/goit-js-hw-09@c1595bfefa07f0916b9de44a33c8fa150f8db518 🚀

--- 02-timer.d7c1d729.js
+++ 02-timer.d7c1d729.js
@@ -17,6 +17,6 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./types/options":"lJVm","./l10n/default":"SFEe","./utils":"U4Vg","./utils/dom":"UwHZ","./utils/dates":"viQd","./utils/formatting":"KtbI","./utils/polyfills":"pFfd"}],"c0Wa":[function(require,module,exports) {
 
 },{}],"d2uN":[function(require,module,exports) {
-"use strict";var e=t(require("flatpickr"));function t(e){return e&&e.__esModule?e:{default:e}}require("flatpickr/dist/flatpickr.min.css");const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};
+"use strict";var e=o(require("flatpickr"));function o(e){return e&&e.__esModule?e:{default:e}}require("flatpickr/dist/flatpickr.min.css");const t={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};function l(e){return{days:Math.floor(e/864e5),hours:Math.floor(e%864e5/36e5),minutes:Math.floor(e%864e5%36e5/6e4),seconds:Math.floor(e%864e5%36e5%6e4/1e3)}}console.log(l(2e3)),console.log(l(14e4)),console.log(l(2414e4));
 },{"flatpickr":"BKNP","flatpickr/dist/flatpickr.min.css":"c0Wa"}]},{},["d2uN"], null)
-//# sourceMappingURL=/goit-js-hw-09/02-timer.1db7a3a5.js.map
+//# sourceMappingURL=/goit-js-hw-09/02-timer.d7c1d729.js.map
